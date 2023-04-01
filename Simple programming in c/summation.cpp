fa70073849f9f6bc3822311f24002f2e992ece66
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-04-01 22:47:21 +0600

    first commit

---
+++ Simple programming in c/summation.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() // main function for the program
+{
+    int a, b;              // variable to store value
+    cin >> a >> b;         // input from user
+    cout << a + b << endl; // output in the program
+}
